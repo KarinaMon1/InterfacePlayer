9c4238561a08a156a30a75d71f1827ceee4163b5
Karina Mongileva <mongilevakarina@gmail.com>
2024-02-24 23:27:45 +0300

    Добавьте файлы проекта.

---
+++ Audioplayer/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace Audioplayer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.stopBtn = new System.Windows.Forms.Button();
+            this.prevBtn = new System.Windows.Forms.Button();
+            this.nextBtn = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.LabelForSong = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // stopBtn
+            // 
+            this.stopBtn.Image = global::Audioplayer.Properties.Resources.playStop;
+            this.stopBtn.Location = new System.Drawing.Point(152, 291);
+            this.stopBtn.Name = "stopBtn";
+            this.stopBtn.Size = new System.Drawing.Size(57, 57);
+            this.stopBtn.TabIndex = 0;
+            this.stopBtn.UseVisualStyleBackColor = true;
+            this.stopBtn.Click += new System.EventHandler(this.stopBtn_Click);
+            // 
+            // prevBtn
+            // 
+            this.prevBtn.Image = global::Audioplayer.Properties.Resources.prev;
+            this.prevBtn.Location = new System.Drawing.Point(51, 291);
+            this.prevBtn.Name = "prevBtn";
+            this.prevBtn.Size = new System.Drawing.Size(54, 57);
+            this.prevBtn.TabIndex = 3;
+            this.prevBtn.UseVisualStyleBackColor = true;
+            this.prevBtn.Click += new System.EventHandler(this.prevBtn_Click);
+            // 
+            // nextBtn
+            // 
+            this.nextBtn.Image = global::Audioplayer.Properties.Resources.next;
+            this.nextBtn.Location = new System.Drawing.Point(249, 291);
+            this.nextBtn.Name = "nextBtn";
+            this.nextBtn.Size = new System.Drawing.Size(52, 57);
+            this.nextBtn.TabIndex = 4;
+            this.nextBtn.UseVisualStyleBackColor = true;
+            this.nextBtn.Click += new System.EventHandler(this.nextBtn_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.PowderBlue;
+            this.pictureBox1.Location = new System.Drawing.Point(21, 28);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(307, 161);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.BackColor = System.Drawing.Color.SteelBlue;
+            this.trackBar1.Location = new System.Drawing.Point(30, 240);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(307, 45);
+            this.trackBar1.TabIndex = 8;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // LabelForSong
+            // 
+            this.LabelForSong.AutoSize = true;
+            this.LabelForSong.BackColor = System.Drawing.Color.Lavender;
+            this.LabelForSong.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.LabelForSong.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.LabelForSong.Location = new System.Drawing.Point(40, 202);
+            this.LabelForSong.Name = "LabelForSong";
+            this.LabelForSong.Size = new System.Drawing.Size(65, 24);
+            this.LabelForSong.TabIndex = 9;
+            this.LabelForSong.Text = "Песня";
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.PowderBlue;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(380, 28);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(271, 303);
+            this.listBox1.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MediumPurple;
+            this.BackgroundImage = global::Audioplayer.Properties.Resources.back3;
+            this.ClientSize = new System.Drawing.Size(680, 371);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.LabelForSong);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.nextBtn);
+            this.Controls.Add(this.prevBtn);
+            this.Controls.Add(this.stopBtn);
+            this.Name = "Form1";
+            this.Text = "Плеер";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button stopBtn;
+        private System.Windows.Forms.Button prevBtn;
+        private System.Windows.Forms.Button nextBtn;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label LabelForSong;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Audioplayer/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Audioplayer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void stopBtn_Click(object sender, EventArgs e)
+        {
+            //остановка/продлжение трека 
+
+
+        }
+
+        private void prevBtn_Click(object sender, EventArgs e)
+        {
+            //переходит к предидущему треку
+
+
+        }
+
+        private void nextBtn_Click(object sender, EventArgs e)
+        {
+            //переходит к следующему треку 
+
+
+        }
+
+        
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            //в таблице к каждой песне привязана картинка типа "BabePic.png" как просто строка
+            //её вытаскиваем и ищем картинку в папке с картинками и вставляем 
+
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            //каким-то образом тут должна работать перемотка по треку 
+        }
+    }
+}
